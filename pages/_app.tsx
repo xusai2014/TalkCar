cbaafe7fa2d3ea5fe5a958aba815759ae0c2869d
jerryxu <jerryxu521@gmail.com>
2020-12-05 20:37:42 +0800

    Please enter the commit message for your changes. Lines starting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,8 +27,8 @@ export default class MyApp extends App {
         }).then((result)=>{
             const { timestamp, nonceStr, signature } = result.data.data;
 
-            wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            window.wx.config({
+                debug: process.env.NODE_ENV === 'development', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: 'wx4268bca2f6420e29', // 必填，公众号的唯一标识
                 timestamp, // 必填，生成签名的时间戳
                 nonceStr, // 必填，生成签名的随机串
@@ -45,6 +45,7 @@ export default class MyApp extends App {
             this.setState({loadStart:true});
             this.init();
         }
+       
 
     }
 
