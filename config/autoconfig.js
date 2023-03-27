da38d0210f8df140685be9cb7e27e5ae310beacc
Jerry <18800102517@163.com>
2023-03-27 16:20:55 +0800

    全站替换域名

--- config/autoconfig.js
+++ config/autoconfig.js
@@ -1,4 +1,4 @@
 export const navList = []
 
-export const baseUrl = process.env.NODE_ENV === 'development'?'https://www.chejj.cc':'https://www.chejj.cc'
-export const Server_baseUrl = process.env.NODE_ENV === 'development'?'https://www.chejj.cc':'http://api_service'
+export const baseUrl = process.env.NODE_ENV === 'development'?'https://www.flygpt.cc':'https://www.flygpt.cc'
+export const Server_baseUrl = process.env.NODE_ENV === 'development'?'https://www.flygpt.cc':'http://api_service'
